d7ed1502137b88482b59a09507797a22326ac5ac
Alexey <akurzov41@gmail.com>
2024-09-08 14:54:16 +0300

    Initial

---
+++ src/src.cmake
@@ -0,0 +1,17 @@
+# Рекурсивно добавляем исходные файлы в проект
+function(add_sources_recursive dir)
+    file(GLOB_RECURSE sources ${dir}/*.cpp ${dir}/*.c)
+    list(APPEND all_sources ${sources})
+
+    set(SOURCES ${SOURCES} ${sources} PARENT_SCOPE)
+
+    foreach(subdir IN LISTS ARGV)
+        if(IS_DIRECTORY ${dir}/${subdir})
+            add_sources_recursive(${dir}/${subdir})
+        endif()
+    endforeach()
+endfunction()
+
+# Добавляем исходные файлы в проект
+set(SOURCES "")
+add_sources_recursive(${CMAKE_CURRENT_SOURCE_DIR}/src)
