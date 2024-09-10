86741f63ad129527114b9af90ec5f2645a951400
Alexey <akurzov41@gmail.com>
2024-09-10 19:32:26 +0300

    Initial tmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,121 @@
-#include <glm/glm.hpp>
-#include <GLFW/glfw3.h>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/transform.hpp>
 #include <iostream>
+#include <functional>
+#include <string>
 
-int main()
+// Подключение GLEW до GLFW
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <GLFW/glfw3.h>
+
+#include <math.h>
+#include <unistd.h>
+
+#include "lab1/settings.hpp"
+
+void check(bool error, const std::string & msg, std::function< void(void) > todo = {}) noexcept(false)
 {
-  if (!glfwInit())
+  if (error)
   {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
+    todo();
+    throw std::logic_error(msg);
   }
+}
 
-  glfwDefaultWindowHints();
+float randf()
+{
+  return static_cast< float >(rand()) / static_cast< float >(RAND_MAX);
+}
 
-  GLFWwindow * window = glfwCreateWindow(300, 300, "Red Triangle", nullptr, nullptr);
-  if (window == nullptr)
-  {
-    std::cerr << "Failed to open GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
+void setupViewport(GLFWwindow * window);
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode);
+void display();
 
+int main(int argc, char ** argv)
+{
+  // Инициализация GLFW
+  check(!glfwInit(), "GLFW Bad initialization");
+
+  // Задается минимальная требуемая версия OpenGL.
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Мажорная
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Минорная
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Установка профайла для которого создается контекст
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // Выключение возможности изменения размера окна
+
+  // Окно
+  GLFWwindow * window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "Lab1", nullptr, nullptr);
+  check(window == nullptr, "Failed to create GLFW window", []() { glfwTerminate(); });
   glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
-  glfwShowWindow(window);
 
-  // Важно! Не эквивалентно glEnable(GL_DEPTH_TEST | GL_DOUBLEBUFFER)
+  // Инициализация GLEW
+  glewExperimental = GL_TRUE;
+  check(glewInit() != GLEW_OK, "Failed to initialize GLEW");
+
+  // Инициализация GLUT
+  glutInit(&argc, argv);
+
+  setupViewport(window);
+
+  // Программа
+
+  glfwSetKeyCallback(window, key_callback);
+
   glEnable(GL_DEPTH_TEST);
-  glEnable(GL_DOUBLEBUFFER);
-  glDepthFunc(GL_LESS);
 
-  glClearColor(0, 0, 0, 1);
-
-  glm::mat4 m = glm::perspective(45.0f, 4.0f / 3.0f, 1.0f, 100.0f);
-  glMatrixMode(GL_PROJECTION);
-  glLoadMatrixf(glm::value_ptr(m));
-
-  while (glfwWindowShouldClose(window) == GL_FALSE)
+  // Цикл отрисовки
+  while (!glfwWindowShouldClose(window))
   {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor4f(1, 0, 0, 1);
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0, 0.5, -5);
-    glVertex3f(0.5, -0.5, -5);
-    glVertex3f(-0.5, -0.5, -5);
-    glEnd();
-
-    glfwSwapBuffers(window);
     glfwPollEvents();
+    // == Начало отрисовки
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.1f, 0.1f, 0.1f, 1);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glColor3f(1.0, 0.0, 0.0);
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Перемещение камеры назад, чтобы чайник был виден
+    glutSolidTeapot(2.5);
+
+    // == Конец  отрисовки
+    glfwSwapBuffers(window);
   }
 
   glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
+}
+
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+  gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  glColor3f(1.0, 0.0, 0.0); // Установка цвета чайника
+  glutSolidTeapot(1.0);     // Отрисовка чайника
+  glFlush();
+}
+
+void setupViewport(GLFWwindow * window)
+{
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  // Нужно передать OPENGL размеры нашего окна
+  glViewport(0, 0, width, height);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45.0, (double)width / (double)height, 0.1, 100.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
 }
